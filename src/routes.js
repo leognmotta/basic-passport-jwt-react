d89954e26b6404377021c5faccde0adf36ac45a8
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-15 08:21:06 -0200

    Export custom routes components to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { isAuthenticated } from './services/auth';
+import { IsLogged, PrivateRoute } from './helper/CustomRouteComponent';
 
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import App from './pages/App/App';
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: '/login', state: { from: props.location } }}
-        />
-      )
-    }
-  />
-);
-
-const IsLogged = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      isAuthenticated() ? (
-        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-      ) : (
-        <Component {...props} />
-      )
-    }
-  />
-);
 
 const Routes = () => (
   <BrowserRouter>
